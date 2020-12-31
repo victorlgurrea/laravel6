1feee1022fe5eff70ae9e4feb2d2f2fd4434aff7
victorlgurrea <victorlgurrea@gmail.com>
2020-12-31 07:58:30 +0100

    proteger modulo dashboard

--- app/Http/Controllers/dashboard/PostController.php
+++ app/Http/Controllers/dashboard/PostController.php
@@ -11,6 +11,11 @@ use App\PostImage;
 
 class PostController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     /**
      * Display a listing of the resource.
      *
--- resources/views/dashboard/partials/nav-header-main.blade.php
+++ resources/views/dashboard/partials/nav-header-main.blade.php
@@ -20,10 +20,15 @@
      
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link" href="" title="login"><span class="sr-only"></span><i class="fas fa-user"></i></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="" title="login"><span class="sr-only"></span><i class="fas fa-user"></i></a>
+                <a class="nav-link" href="{{ route('logout') }}"
+                onclick="event.preventDefault();
+                              document.getElementById('logout-form').submit();">
+                 {{ __('Logout') }}
+                </a>
+
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                  @csrf
+              </form>
             </li>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
